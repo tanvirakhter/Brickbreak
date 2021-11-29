5268f715ee897a998858afd270fc9bac18e1ba27
Tanvir Akhter Shakib <2013158@brunel.ac.uk>
2021-11-29 02:53:57 +0000

    My fun project

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+import java.awt.Color;
+
+import javax.swing.JFrame;
+
+public class Main {
+	/**
+	 * 
+	 * 
+	 * @author tanvirakhtershakib
+	 *
+	 *
+	 */
+	public static void main(String[] args) {
+		JFrame obj=new JFrame();
+		gameplay gamePlay = new gameplay();
+		
+		obj.setBounds(10, 10, 700, 600);
+		obj.setTitle("অদ্ভুত গেইম");		
+		obj.setResizable(false);
+		obj.setVisible(true);
+		obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		obj.add(gamePlay);
+                obj.setVisible(true);
+		
+	}
+
+}
